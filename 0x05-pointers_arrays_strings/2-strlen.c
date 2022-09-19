b36fc99adcc4b938642b813c97763974acd4e510
Irnes Dube <nonhlanhladube05@gmail.com>
2022-09-18 23:28:53 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strien - function which returns length of a string
+ * @s: s is a character
+ * Return: i is value
+ */
+int _strien(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
