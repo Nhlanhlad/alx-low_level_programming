0340159421640b5a0a52d0558b06929699ab8368
Irnes Dube <nonhlanhladube05@gmail.com>
2022-09-22 06:41:06 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,14 +12,21 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i = 0, c = 0, n = 0;
 
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for(index = 0; src[index]; index++)
-		dest[dest_len+=] = src[index];
-
-	return(dest);
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[c] != '\0')
+	{
+		c++;
+	}
+	while (n <= c)
+	{
+		dest[i] = src[n];
+		n++;
+		i++;
+	}
+	return (dest);
 }
