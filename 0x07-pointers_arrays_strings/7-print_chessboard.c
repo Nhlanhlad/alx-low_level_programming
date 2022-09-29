19731ad77269b16a60de24c0a9453a3244100359
Irnes Dube <nonhlanhladube05@gmail.com>
2022-09-29 07:01:31 -0700

    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,10 +5,9 @@
  * @a: array
  * Return: Always 0.
  */
-void print_chessboard(char (*a)[8]);
-{
-	int i;
-	int j;
+void print_chessboard(char (*a)[8])
+
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
